3780f05a228035f8e348cc331082f155b58d200a
RAMMLS <robertr.shakirov@bk.ru>
2025-10-17 09:58:22 +0300

    Written MAC, IP, Phys adresses finding

--- Level_3/PacketCaptureTool/main.c
+++ Level_3/PacketCaptureTool/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+
 u_char* learn_IP()
 {
 	addrinfo hnts, *pRes;
@@ -18,4 +21,24 @@ u_char* learn_IP()
 	WSACleanup();
 	}
 	return ip_addr;
+}
+
+u_char *learn_MAC() {
+  IP_ADAPTER_INFO ip_ainf[128];
+  PIP_ADAPTER_INFO pip_ainf = ip_ainf;
+  u_long bufLen = sizeof(ip_ainf);
+  GetAdaptersInfo(ip_ainf, &bufLen);
+  u_char* mac_addr = pip_ainf -> Address;
+
+  return mac_addr;
+}
+
+IPAddr DestIP = 0, ScrIP = 0;
+static u_long MacAddr[2];
+u_long PhysAddrLen;
+ScrIpAddr = inet_addr(ScriptString);  // IP адрес отправителя
+DestIPAddr = inet_addr(destIpString); // IP адрес маршрутизатора
+memset (&MacAddr, 0xff, sizeof(MacAddr)); // Широковещательная рассылка 
+if (SendAddr (DestIPAddr, ScrIpAddr, &MacAddr, &PhysAddrLen) == NO_ERROR) {
+  *bPhysAddr = (BYTE *) & MacAddr; // Искомый MAC адрес
 }
