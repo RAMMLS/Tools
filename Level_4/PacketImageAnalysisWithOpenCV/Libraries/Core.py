e0684829efa6ce282a059f0948f367d25f571edc
RAMMLS <robertr.shakirov@bk.ru>
2025-10-18 17:14:18 +0300

    Written Core.py

---
+++ Level_4/PacketImageAnalysisWithOpenCV/Libraries/Core.py
@@ -0,0 +1,18 @@
+class Engine:
+    """
+    Движок
+    """
+    pass 
+
+class ImageProcessingStep:
+    """
+    Шаг обработки изображений
+    """
+    pass
+    
+
+class ImageInfo:
+    """
+    Содержимое картинки, включая результаты обработки 
+    """
+    pass 
