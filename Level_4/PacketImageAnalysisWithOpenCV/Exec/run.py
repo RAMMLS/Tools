952ce375f046f3c81a93cce120be292133a1c390
RAMMLS <robertr.shakirov@bk.ru>
2025-10-19 11:07:06 +0300

    Update run.py

--- Level_4/PacketImageAnalysisWithOpenCV/Exec/run.py
+++ Level_4/PacketImageAnalysisWithOpenCV/Exec/run.py
@@ -5,10 +5,16 @@ from Lib.ImageProcessingSteps import MedianBlurProcessingStep
 my_photo = cv2.Imread('~/Tools/Level_4/PacketImageAnalysisWithOpenCV/Photos/MyPhoto1.jpg')
 core = Engine()
 core.steps.append(MedianBlurProcessingStep(5))
-info = core.processing(my_photo)
+res, history = core.process(my_photo)
 
-cv2.imshow('origin', info.image) # вывод исходника
-cv2.imshow('res', info.filtered_image) # вывод итога 
+i = 1
+for info in history:
+    cv2.imshow('image' + str(i), info.image)
+    i = i + 1
+cv2.imshow('res', res.image)
+
+#cv2.imshow('origin', info.image) # вывод исходника
+#cv2.imshow('res', info.filtered_image) # вывод итога 
 
 cv2.waitKey()
 cv2.destroyllWindows()
