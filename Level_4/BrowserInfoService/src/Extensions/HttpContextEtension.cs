5260fc9f38c5ccbc8d6166b26ff549996b8ebbe2
RAMMLS <robertr.shakirov@bk.ru>
2025-10-26 17:03:15 +0300

    Written HttpContext

---
+++ Level_4/BrowserInfoService/src/Extensions/HttpContextEtension.cs
@@ -0,0 +1,8 @@
+namespace BrowserInfoService.Extensions {
+  public static class HttpContextExtensions {
+    public static string? GetRemoteIpAddress (this HttpContext context) {
+
+      return context.Connection.RemoteIpAddress?.ToString();
+    }
+  }
+}
