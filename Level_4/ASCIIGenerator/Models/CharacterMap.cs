0381dcc546d463d0f5349148dd2c8b77d062616e
RAMMLS <robertr.shakirov@bk.ru>
2025-10-28 10:28:30 +0300

    Written Models/

---
+++ Level_4/ASCIIGenerator/Models/CharacterMap.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace AsciiNameGenerator.Models
+{
+    public class CharacterMap
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Dictionary<char, string[]> Characters { get; set; }
+
+        public CharacterMap()
+        {
+            Characters = new Dictionary<char, string[]>();
+        }
+    }
+}
---
+++ Level_4/ASCIIGenerator/Models/GenerationOptions.cs
@@ -0,0 +1,25 @@
+using AsciiNameGenerator.Styles;
+
+namespace AsciiNameGenerator.Models
+{
+    public class GenerationOptions
+    {
+        public string FontName { get; set; } = "Standard";
+        public ColorStyle ColorStyle { get; set; } = ColorStyle.Default;
+        public int Width { get; set; } = 80;
+        public int Height { get; set; } = 25;
+        public bool ShowBorder { get; set; } = true;
+        public bool UseAnimation { get; set; } = false;
+        public int AnimationSpeed { get; set; } = 100;
+
+        public GenerationOptions()
+        {
+        }
+
+        public GenerationOptions(string fontName, ColorStyle colorStyle)
+        {
+            FontName = fontName;
+            ColorStyle = colorStyle;
+        }
+    }
+}
